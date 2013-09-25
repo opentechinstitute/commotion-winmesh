59098f4b7cb1ad99b6762bb300957a155273fa59
Jonathan Nelson <jnelson@eds.org>
2013-09-25 02:37:41 -0400

    sort of works

--- workout.py
+++ workout.py
@@ -14,6 +14,7 @@ import subprocess # for netsh
 
 commotion_BSSID = '12:CA:FF:EE:BA:BE' # shows up in a few Commotion places
 commotion_SSID = 'commotion-wireless.net'
+commotion_profile_name = 'commotion-wireless.net'
 ASADMIN = 'asadmin'
 xml_profile_path = "commotion_wireless_profile.xml"
 
@@ -34,11 +35,6 @@ if cmd_subfolder not in sys.path:
 
 import WindowsWifi as PyWiWi
 
-print 'You must run this script as an administrator'
-
-with open(xml_profile_path, "r") as f_profile:
-    commotion_wlan_profile_xml = "".join(line.rstrip() for line in f_profile)
-
 # scan for existing Commotion wireless networks
 ifaces = PyWiWi.getWirelessInterfaces()
 net_list = []
@@ -51,6 +47,89 @@ for iface in ifaces:
                                       commotion_BSSID) else
                                       "-"})
 
+wmi_ifaces = wmi.WMI().Win32_NetworkAdapter()
+ifaces_by_guid = {}
+for iface in wmi_ifaces:
+    ifaces_by_guid[iface.GUID] = iface
+
+def get_netsh_name(iface):
+    return ifaces_by_guid[str(iface.guid)].NetConnectionID
+
+def make_profile():
+    subprocess.call("".join(["netsh wlan add profile",
+                             " filename=\"",
+                             xml_profile_path,
+                             "\""]))
+
+def connect(netsh_name):
+    subprocess.call("".join(["netsh wlan connect",
+                             " name=\"",
+                             commotion_profile_name,
+                             "\" interface=\"",
+                             netsh_name,
+                             "\""]))
+
+def start_olsrd(netsh_name):
+    subprocess.call("".join(["olsrd.exe",
+                             " -d 2",
+                             " -i \"",
+                             netsh_name,
+                             "\" -f olsrd.conf"]))
+
+def join_network(target_net):
+    target_guid = str(target["interface"].guid)
+    target_net["interface"].netsh_name = get_netsh_name(target_net["interface"])
+    # TODO:
+
+def make_network():
+    # pick an interface to use
+    print "#   Interface"
+    idx = 0
+    for iface in ifaces:
+        print "".join(["{0:>2} ",
+                       "{1.description}"]).format(idx+1, iface)
+        idx = 1 + idx
+    iface_choice = raw_input("Enter the # of the interface to use:\n")
+    target_iface = ifaces[int(iface_choice)-1]
+    target_iface.netsh_name = get_netsh_name(target_iface)
+    # add a profile for commotion
+    # if this profile already exists, it will not be added again
+    make_profile()
+    # connect to the network
+    connect(target_iface.netsh_name)
+    # start olsrd
+    start_olsrd(target_iface.netsh_name)
+    hold_and_finish(target_iface.netsh_name)
+
+def hold_and_finish(netsh_name):
+    # stay connected until done
+    holdup = ''
+    while holdup != '!':
+        holdup = raw_input("Enter ! to disconnect\n")
+
+    # disconnect from current network
+    #PyWiWi.disconnect(target_net["interface"])
+    subprocess.call("".join(["netsh wlan disconnect",
+                             " interface=\"",
+                             netsh_name,
+                             "\""]))
+
+    # show current info for adapter
+
+
+    # go back to old configuration when ready
+    delete_profile = raw_input("Delete Commotion Wireless profile? (Y|N)\n")
+    if delete_profile == 'Y':
+        subprocess.call("".join(["netsh wlan delete profile",
+                                 " name=\"",
+                                 commotion_profile_name,
+                                 "\" interface=\"",
+                                 netsh_name,
+                                 "\""]))
+
+#with open(xml_profile_path, "r") as f_profile:
+    #commotion_wlan_profile_xml = "".join(line.rstrip() for line in f_profile)
+
 # choose desired network
 net_list.sort(key=lambda opt: opt["network"].link_quality, reverse=True)
 print "#  CW? Interface     Qual BSSID             SSID"
@@ -60,59 +139,33 @@ for idx, net in enumerate(net_list):
                    "{1.description:13.13} ",
                    "{2.link_quality:>3}% ",
                    "{2.bssid} ",
-                   "{2.ssid}"]).format(idx,
+                   "{2.ssid}"]).format(idx+1,
                                        net["interface"],
                                        net["network"],
                                        net["commotion"])
-# FIXME: below will just exit due to int if Q or other letter entered
-target_net = net_list[int(raw_input("Enter the # of the network to join or Q to exit: "))]
-
-print "network", target_net["network"]
-print "interface", target_net["interface"]
-
-# add a profile for commotion
-subprocess.call("".join(["netsh wlan add profile",
-                         " filename=commotion_wireless_profile.xml"]))
-
+net_choice = raw_input("".join(["Enter the # of the network to join,\n",
+                           "enter 0 (zero) to start a new network,\n",
+                           "or enter Q to quit:\n"]))
+# FIXME:
+# TODO:
+# Everybody always chooses 0, because not done
+net_choice = 0
+if net_choice > 0 and net_choice <= len(net_list):
+    # join an existing network
+    target_net = net_list[int(choice-1)]
+    join_network(target_net)
+elif net_choice == 0:
+    make_network()
+else:
+    exit()
 # connect to chosen network
 # http://msdn.microsoft.com/en-us/library/windows/desktop/ms706851(v=vs.85).aspx
-cnxp = {"connectionMode": 0,
-        "profile": "commotion_wireless_profile.xml",
-        #"ssid": target_net["network"].ssid,
-        "ssid": None,
-        "bssidList": [target_net["network"].bssid],
-        # bssType must match type from profile provided
-        "bssType": target_net["network"].bss_type,
-        "flags": 0}
+#cnxp = {"connectionMode": 0,
+        #"profile": xml_profile_path,
+        ##"ssid": target_net["network"].ssid,
+        #"ssid": None,
+        #"bssidList": [target_net["network"].bssid],
+        ## bssType must match type from profile provided
+        #"bssType": target_net["network"].bss_type,
+        #"flags": 0}
 #PyWiWi.connect(target_net["interface"], cnxp)
-
-subprocess.call("".join(["netsh wlan connect",
-                         " name=commotion-wireless.net",
-                         " interface=\"",
-                         "Wireless Network Connection",
-                         "\""]))
-
-# stay connected until done
-holdup = ''
-while holdup != '!':
-    holdup = raw_input("Enter ! to disconnect\n")
-
-# disconnect from current network
-#PyWiWi.disconnect(target_net["interface"])
-subprocess.call("".join(["netsh wlan disconnect",
-                         " interface=\"",
-                         "Wireless Network Connection",
-                         "\""]))
-
-# show current info for adapter
-
-
-# go back to old configuration when ready
-delete_profile = raw_input("Delete Commotion Wireless profile? (Y|N)\n")
-if delete_profile == 'Y':
-    subprocess.call("".join(["netsh wlan delete profile",
-                             " name=commotion-wireless.net",
-                             " interface=\"",
-                             "Wireless Network Connection",
-                             "\""]))
-
