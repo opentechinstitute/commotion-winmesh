01e833199b402a85bf7cc0a2e69168c516805789
Jonathan Nelson <jnelson@eds.org>
2013-09-26 17:03:33 -0400

    initial commit for profile template

---
+++ profile_template.xml.py
@@ -0,0 +1,22 @@
+<?xml version="1.0"?>
+<WLANProfile xmlns="http://www.microsoft.com/networking/WLAN/profile/v1">
+	<name>{profile_name}</name>
+	<SSIDConfig>
+		<SSID>
+			<hex>{ssid_hex}</hex>
+			<name>{ssid_name}</name>
+		</SSID>
+		<nonBroadcast>false</nonBroadcast>
+	</SSIDConfig>
+	<connectionType>IBSS</connectionType>
+	<connectionMode>manual</connectionMode>
+	<MSM>
+		<security>
+			<authEncryption>
+				<authentication>open</authentication>
+				<encryption>none</encryption>
+				<useOneX>false</useOneX>
+			</authEncryption>
+		</security>
+	</MSM>
+</WLANProfile>
