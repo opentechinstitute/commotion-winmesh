943a9776151a5b970809d4c47004d94b24d78ee4
Josh Steiner <josh@vitriolix.com>
2013-10-30 19:52:04 -0700

    revert broken path finding with functionally broken path finding for now

--- workout.py
+++ workout.py
@@ -64,8 +64,9 @@ def get_own_path(extends_with=None):
     else:
         extends_with = ""
         sep = ""
-    base_path = os.path.dirname(os.path.abspath(inspect.getfile(
-            inspect.currentframe())))
+    base_path = os.path.dirname(os.path.abspath(sys.argv[0]))
+    #base_path = os.path.dirname(os.path.abspath(inspect.getfile(
+    #        inspect.currentframe())))
     ext_path = os.path.abspath("".join([base_path, sep, extends_with]))
     return ext_path
 
